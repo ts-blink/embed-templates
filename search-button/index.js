de222df88225bc06df6e040db7e6be9ef73d57a3
Bhanu Jupally <bhanu.jupally@thoughtspot.com>
2020-11-06 03:09:34 +0530

    Added search popover button

---
+++ search-button/index.js
@@ -0,0 +1,32 @@
+// Import stylesheets
+import "./styles.css";
+import { init, SearchEmbed } from "ts-embed-sdk";
+
+// Write Javascript code!
+init({
+  thoughtSpotHost: "https://172.18.92.4:8443/v2",
+  authType: "SSO"
+});
+
+document.getElementById("openSearch").addEventListener("click", () => {
+  let tsSearch = new SearchEmbed(".popover .popover-body", {
+    frameParams: {}
+  });
+  tsSearch.on("init", showLoader).on("load", hideLoader).render();
+  document.querySelector(".popover").classList.add("open");
+});
+
+document
+  .querySelector(".popover .popover-header .close-btn")
+  .addEventListener("click", () => {
+    document.querySelector(".popover").classList.remove("open");
+    document.querySelector(".popover .popover-body iframe").remove();
+  });
+
+function showLoader() {
+  document.getElementById("loader").style.display = "block";
+}
+function hideLoader() {
+  document.getElementById("loader").style.display = "none";
+}
+

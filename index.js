81a84e14bb2491f921265cfb6dc586d11c3d4f8b
Bhanu Jupally <bhanu.jupally@thoughtspot.com>
2020-11-17 00:27:32 +0530

    added hosturl param

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { init, SearchEmbed } from "ts-embed-sdk";
 
 // Write Javascript code!
 init({
-  thoughtSpotHost: "https://172.18.92.4:8443/v2",
+  thoughtSpotHost: /*param-start-hosturl*/"https://172.18.92.4:8443"/*param-end-hosturl*/,
   authType: "SSO"
 });
 

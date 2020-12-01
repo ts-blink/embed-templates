1ee49169f5a1e5ed70d85d17fc4d2aa8fae90db0
Ayon Ghosh <ayon.ghosh@thoughtspot.com>
2020-12-01 18:33:05 +0530

    Add code comments

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-// Import packages
-import "./styles.css";
+// Import ThoughtSpot SDK
 import { init, PinboardEmbed } from "ts-embed-sdk";
+import "./styles.css";
 
-// Write Javascript code!
+// Initialize embed configuration
 init({
   thoughtSpotHost:
     /*param-start-hosturl*/"https://172.18.92.4:8443"/*param-end-hosturl*/,
   authType: "SSO"
 });
 
+// Instantiate class to embed a visualization
 const embed = new PinboardEmbed("#embed", {
   frameParams: {
     width: 1280,
     height: 720
   }
 });
+
 embed
+  // Register event listeners
   .on("init", showLoader)
   .on("load", hideLoader)
+  // Render a viz within a pinboard
   .render(
-    "21329019-3cb1-475e-b159-56b875150c61",
-    "91e54454-b8ed-4e40-8404-d40216369521"
+    "2f64357c-d750-43a7-8a14-8d6f9c885efe",
+    "3454ebf1-a5f2-437d-87d0-07c47eaef5b3"
   );
 
+// Functions to show/hide a loader while the iframe loads
 function showLoader() {
   document.getElementById("loader").style.display = "block";
 }

dcd798936d0d2dbb0a4fa1381051f623f1cf07bd
Ayon Ghosh <ayon.ghosh@thoughtspot.com>
2020-12-01 18:23:03 +0530

    Add data sources selection

--- index.js
+++ index.js
@@ -8,21 +8,35 @@ init({
     /*param-start-hosturl*/ "https://172.18.92.4:8443" /*param-end-hosturl*/,
   authType: "SSO"
 });
+
+// Instantiate SearchEmbed class
 const tsSearch = new SearchEmbed("#embed", {
-  frameParams: {}
+  frameParams: {},
+  // Configure theme colours
+  theme: {
+    backgroundColor: '#C6E3CF'
+  }
 });
-console.log(tsSearch.getId());
+
 tsSearch
+  // Register event handlers
   .on("init", showLoader)
   .on("load", hideLoader)
   .on("answerPageLoading", payload =>
     console.log("message received from embedded view" + JSON.stringify(payload))
   )
-  .render(["8dada323-5956-4e2d-ae12-81bfc78b2ea5"]); // data source;
-document.getElementById("message").addEventListener("click", () => {
-  tsSearch.trigger("update", { data: "Hello from parent" });
-});
+  // Render the embedded search and pass in the data source id
+  .render(["8dada323-5956-4e2d-ae12-81bfc78b2ea5"]);
 
+// On selecting a data source, trigger an event to ThoughtSpot
+// to change the search data source
+document.getElementById("data-source")
+  .addEventListener("change", (e) => {
+    const value = e.target.options[e.target.selectedIndex].value;
+    tsSearch.trigger('setDataSources', [value]);
+  });
+
+// Show/hide a loader while iframe is loading
 function showLoader() {
   document.getElementById("loader").style.display = "block";
 }

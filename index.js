2515652e38e123ed8f2f15a9c1c2ee47624d3e12
Bhanu Jupally <bhanu.jupally@thoughtspot.com>
2021-02-01 22:21:21 +0530

    Updated sdk to 0.0.1-alpha.6

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ embed
   .on("init", showLoader)
   .on("load", hideLoader)
   // Render pinboard with UUID
-  .render(/*param-start-pinboardId*/"976bb48e-b1e0-4621-a7da-51161cef4b29"/*param-end-pinboardId*/);
+  .render({
+      /*param-start-pinboardId*/pinboardId: "976bb48e-b1e0-4621-a7da-51161cef4b29",/*param-end-pinboardId*/
+      runtimeFilters: [],
+  });
 
 // Functions to show and hide a loader while iframe loads
 function showLoader() {

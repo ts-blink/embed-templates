e8cff00f421c3fcecef341eb66f73291675f2040
Bhanu Jupally <bhanu.jupally@thoughtspot.com>
2021-02-01 22:24:04 +0530

    Updated sdk to 0.0.1-alpha.6

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ embed
   .on("init", showLoader)
   .on("load", hideLoader)
   // Render a viz within a pinboard
-  .render(
-    /*param-start-pinboardId*/"976bb48e-b1e0-4621-a7da-51161cef4b29"/*param-end-pinboardId*/,
-    /*param-start-answerId*/"b53ed4b9-7a95-4a16-9f6b-1c7f53d9777f"/*param-end-answerId*/
-  );
+  .render({
+      /*param-start-pinboardId*/pinboardId: "976bb48e-b1e0-4621-a7da-51161cef4b29",/*param-end-pinboardId*/
+    /*param-start-answerId*/vizId: "b53ed4b9-7a95-4a16-9f6b-1c7f53d9777f",/*param-end-answerId*/
+      runtimeFilters: [],
+  });
 
 // Functions to show/hide a loader while the iframe loads
 function showLoader() {

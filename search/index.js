8f9321c56764f0d0a735e7d7690e8102ede53419
Bhanu Jupally <bhanu.jupally@thoughtspot.com>
2021-03-11 10:46:40 +0530

    SCAL-77678 - Added customAction snippet

--- search/index.js
+++ search/index.js
@@ -30,6 +30,7 @@ tsSearch
   // Register event handlers
   .on("init", showLoader)
   .on("load", hideLoader)
+  /*param-start-customActionHandle*//*param-end-customActionHandle*/
   .on("answerPageLoading", payload =>
     console.log("message received from embedded view" + JSON.stringify(payload))
   )
